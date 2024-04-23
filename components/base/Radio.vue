e8516aa4c2bc94f64a8dc26e6c7de7b019b81566
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-04-23 23:52:09 +0300

    complete company account

---
+++ components/base/Radio.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="form-control">
+    <label class="label cursor-pointer">
+      <span class="label-text me-4 mb-2">{{ label }}</span>
+      <input
+        type="radio"
+        name="radio-10"
+        class="radio radio-primary"
+        checked
+      />
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  label: string
+}>()
+</script>
