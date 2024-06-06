c11e83c76f413ff2666e18ee951a29c008f3db8b
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-06-06 18:35:38 +0300

    add components

---
+++ components/company/InlineCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex items-center">
+       <div class="w-10 h-10 rounded-full bg-slate-200"></div>
+       
+       <div class="ms-2">
+           <div class="font-semibold text-md text-gray-700">شركة المرعبين المحدودة</div>
+           <div class="font-semibold text-md text-gray-400">شركة برمجية</div>
+       </div>
+
+       <div class="flex-grow"></div>
+
+       <base-chip color="secondary" class="!badge-md">4 عروض</base-chip>
+    </div>
+</template>
---
+++ components/offer/Stat.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card shadow-md p-4 rounded-lg">
+    <div class="text-2xl mb-4">أبرز الشركات</div>
+
+    <company-inline-card></company-inline-card>
+
+    <div class="divider w-[80%] mx-auto"></div>
+
+    <company-inline-card></company-inline-card>
+    <div class="divider w-[80%] mx-auto"></div>
+
+    <company-inline-card></company-inline-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+const types = ref([
+  {
+    name: 'برمجي',
+    count: 305
+  },
+  {
+    name: 'طبي',
+    count: 20
+  },
+  {
+    name: 'خدمي',
+    count: 100
+  },
+  {
+    name: 'صناعي',
+    count: 150
+  }
+])
+</script>
