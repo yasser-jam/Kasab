a485bc63eff33ab0c9c3ffee1b92745c5e783001
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-06-17 12:00:25 +0300

    add base modal

---
+++ components/base/Modal.vue
@@ -0,0 +1,16 @@
+<template>
+  <dialog id="my_modal_3" class="modal">
+    <div class="modal-box bg-slate-50">
+      <form method="dialog">
+        <button class="btn btn-md btn-circle btn-ghost absolute left-2 top-2">
+          ✕
+        </button>
+      </form>
+      <h3 class="font-bold text-xl">عنوان</h3>
+    </div>
+  </dialog>
+</template>
+
+<script setup lang="ts">
+const el = document.querySelector('#my_modal_3')
+</script>
--- pages/offers.vue
+++ pages/offers.vue
@@ -28,4 +28,6 @@
       </div>
     </div>
   </div>
+
+  <base-modal></base-modal>
 </template>
