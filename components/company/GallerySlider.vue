95b8aa839663ccefebcedda0af4d844c0e45f24f
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-06-21 01:40:09 +0300

    fix gallery

--- components/company/GallerySlider.vue
+++ components/company/GallerySlider.vue
@@ -1,28 +1,31 @@
 <template>
-  <div class="flex items-center gap-4">
-    <Swiper
-      :slides-per-view="4"
-      space-between="50"
-      :loop="true"
-      class="hidden bg-gray-100 px-8"
-    >
-      <SwiperSlide v-for="img in images" :key="img" class="">
-        <div class="relative w-44 h-44 group">
-          <img
-            :src="`${img}`"
-            alt="gallery-image"
-            class="object-contain bg-gray-50 h-full w-full relative block"
-          />
+  <div class="flex items-center gap-4 overflow-hidden">
+    <div class="overflow-hidden">
+      <Swiper
 
-          <span
-            class="hidden group-hover:flex absolute top-0 left-0 justify-center items-center w-full h-full bg-black/50 rounded-lg cursor-pointer"
-            @click="removeImage(img)"
-            >
-            <Icon name="mdi:close" class="text-error" size="3rem" />
-          </span>
-        </div>
-      </SwiperSlide>
-    </Swiper>
+        :slides-per-view="4"
+        space-between="50"
+        :loop="false"
+        class="hidden bg-gray-100 px-8"
+      >
+        <SwiperSlide v-for="img in images" class="">
+          <div class="relative w-44 h-44 group">
+            <img
+              :src="`${img}`"
+              alt="gallery-image"
+              class="object-contain bg-gray-50 h-full w-full relative block"
+            />
+  
+            <span
+              class="hidden group-hover:flex absolute top-0 left-0 justify-center items-center w-full h-full bg-black/50 rounded-lg cursor-pointer"
+              @click="removeImage(img)"
+              >
+              <Icon name="mdi:close" class="text-error" size="3rem" />
+            </span>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
 
     <base-image-uploader v-model="image" @update:model-value="addImage"></base-image-uploader>
   </div>
