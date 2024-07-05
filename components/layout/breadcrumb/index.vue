d322ab9c14148027ca616c696bf5d1400f4c7ed8
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-07-05 19:27:54 +0300

    clean treee

--- components/layout/breadcrumb/index.vue
+++ components/layout/breadcrumb/index.vue
@@ -2,8 +2,9 @@
   <div class="text-lg font-semibold py-0 breadcrumbs overflow-hidden">
     <ul>
       <li><nuxt-link to="/">الصفحة الرئيسية</nuxt-link></li>
-      <li><nuxt-link to="/">الحسابات</nuxt-link></li>
-      <li><nuxt-link to="/my-account">حساب الشركة</nuxt-link></li>
+      <li><nuxt-link to="/">معلومات الموظف</nuxt-link></li>
+      <li><nuxt-link to="/my-account">معرض الأعمال</nuxt-link></li>
+      <li><nuxt-link to="/my-account">اسم العمل</nuxt-link></li>
     </ul>
   </div>
 </template>
