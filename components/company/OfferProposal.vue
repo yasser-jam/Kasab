f852011e65989ce6c0dd045d824f10d6a7e1c692
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-07-07 22:25:40 +0300

    offer proposal card

---
+++ components/company/OfferProposal.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card">
+        <div class="flex items-center gap-4">
+            <div class="w-12 h-12 rounded-full bg-gray-200"></div>
+    
+            <div class="">
+                <div class="flex gap-2 items-center mb-2">
+                    <nuxt-link class="text-lg font-semibold" to="/employee/2">
+                        اسم المستقل
+                    </nuxt-link>
+    
+                    <base-rating></base-rating>
+                </div>
+    
+                <div class="flex gap-2 items-center text-gray-500 font-semibold text-sm">
+                    <div class="flex items-center">
+                        <Icon class="me-2" name="mdi:briefcase-outline" />
+                        <div>مطور واجهات أمامية</div>
+                    </div>
+    
+                    <div class="flex items-center">
+                        <Icon class="me-2" name="mdi:clock-outline" />
+                        <div>منذ ثلاث دقائق</div>
+                    </div>
+                </div>
+            </div>
+    
+            
+        </div>
+
+    </div>
+</template>
--- pages/company/[company_id]/offers/index.vue
+++ pages/company/[company_id]/offers/index.vue
@@ -3,11 +3,13 @@
       <layout-breadcrumb class="my-8"></layout-breadcrumb>
   
       <div class="flex justify-between items-center mb-8">
-        <base-title>عروضي</base-title>
+        <base-title class="font-bold">عروضي</base-title>
         <base-btn color="neutral" icon="mdi:domain" to="/company/1">معلومات الشركة</base-btn>
       </div>
   
-      <offer-card class="mb-4"></offer-card>
+      <nuxt-link to="/company/1/offers/1">
+        <offer-card class="mb-4"></offer-card>
+      </nuxt-link>
     </div>
   
     <!-- <NuxtPage /> -->
